419ed27786b2f5ed57533eb4ea4b52c5010de537
Schweiker <maximilianschweiker@gmail.com>
2014-10-29 14:09:29 +0100

    my first commit
    This is my first commit to set up my repository fpr algorithm
    Engineering

---
+++ SecondExcercise/Fibonacci.cpp
@@ -0,0 +1,13 @@
+/*
+ * Fibonacci.cpp
+ *
+ *  Created on: 29.10.2014
+ *      Author: Schweiker
+ */
+
+#include "Fibonacci.h"
+
+class Fibonacci
+{
+
+}
---
+++ SecondExcercise/Fibonacci_test.cpp
@@ -0,0 +1,9 @@
+/*
+ * Fibonacci_test.cpp
+ *
+ *  Created on: 29.10.2014
+ *      Author: Schweiker
+ */
+
+#include "Fibonacci.cpp"
+
