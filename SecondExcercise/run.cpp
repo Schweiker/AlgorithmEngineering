ea1f6ba38c245bceb20b51cd2d44ddd81c907583
Schweiker <maximilianschweiker@gmail.com>
2014-10-30 12:39:23 +0100

    This commit is the same as my last commit, but i had trouble with github so i commited a second time.

--- SecondExcercise/run.cpp
+++ SecondExcercise/run.cpp
@@ -1,6 +1,6 @@
 #include "Fibonacci.h"
 #include <iostream>
-<<<<<<< HEAD
+
 #include "gtest/gtest.h"
 
 
@@ -18,16 +18,3 @@ int main(int argc, char **argv)
 	std::cout << f.calculateNthFibonacciNumber(7);
 
 return RUN_ALL_TESTS();
-=======
-
-
-
-int main()
-{
-	Fibonacci f;
-
-	std::cout << f.calculateNthFibonacciNumber(1);
-
-return 0;
->>>>>>> 0f7639ace315b7dd1425879c2e72158558d85012
-};
